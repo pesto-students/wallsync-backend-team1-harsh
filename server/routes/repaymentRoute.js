8f5eb762a9839a67ee70489b1f5f3cdfd5f75f18
mrmissong <mrmissong@gmail.com>
2023-02-24 00:04:58 +0530

    repayment api

--- server/routes/repaymentRoute.js
+++ server/routes/repaymentRoute.js
@@ -5,5 +5,5 @@ const repaymentController = require("../controllers/repaymentController");
 router.post("/:id/addRepayment", repaymentController.addRepayment);
 router.put("/:id/:repaymentId/edit", repaymentController.editRepayment);
 router.delete("/:id/:repaymentId/delete", repaymentController.deleteRepayment);
-
+router.get("/:id/repayments", repaymentController.getRepayments);
 module.exports = router;
