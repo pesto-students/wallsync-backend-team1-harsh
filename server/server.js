71fb32af6b18726de22bf4ffc04a4244f7eebf4c
mrmissong <mrmissong@gmail.com>
2023-03-18 11:50:27 +0530

    vercel.json

--- server/server.js
+++ server/server.js
@@ -22,6 +22,12 @@ app.use("/api", groupRoute);
 app.use("/api", contributionRoute);
 app.use("/api", budgetRoute);
 app.use("/api", repaymentRoute);
+app.get("/api", (req, res) => {
+	const path = `/api/item/${v4()}`;
+	res.setHeader("Content-Type", "text/html");
+	res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+	res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 app.get(
 	"/",
 	(req,
